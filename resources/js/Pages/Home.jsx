8831d710c725e291be12906841a619c4952b087d
Tenarien <mamateki7@gmail.com>
2024-09-04 19:51:52 +0100

    Created basic home page with posts and pagination

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,40 @@
+import Layout from "@/Layouts/Layout.jsx";
+import {Link} from "@inertiajs/react";
+
+export default function Home({ posts }) {
+    return (
+        <>
+            <h1 className="text-3xl">Hello</h1>
+
+            <div>
+                {posts.data.map(post => (
+                    <div key={post.id} className="p-4 border-b border-gray-200">
+                        <span className="font-bold">Posted: </span>
+                        <span className="text-sm">{ new Date(post.created_at).toLocaleTimeString()}</span>
+                        <p className="font-medium">{post.body}</p>
+                    </div>
+                ))}
+            </div>
+            <div className="flex justify-center mt-4">
+                { posts.links.map(link => (
+                        link.url ?
+                        <Link
+                            key={link.label}
+                            href={link.url}
+                            dangerouslySetInnerHTML={{__html: link.label}}
+                            className={`p-1 mx-1 ${ link.active ? "text-blue-500 font-bold" : ""
+                            }`}
+                        />
+                        :
+                        <span
+                            key={link.label}
+                            dangerouslySetInnerHTML={{__html: link.label}}
+                            className="p-1 mx-1 opacity-0"
+                        >
+                        </span>
+                ))}
+            </div>
+        </>
+    );
+}
+
