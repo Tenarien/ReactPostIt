cefa64b46322d17edc2a495ad053139e71e33303
Tenarien <mamateki7@gmail.com>
2024-09-05 19:43:57 +0100

    Added show post component with delete and edit functionality

---
+++ resources/js/Pages/Show.jsx
@@ -0,0 +1,38 @@
+import {Head, Link, useForm, usePage} from "@inertiajs/react";
+
+export default function Show({ post }) {
+    const {delete: destroy, processing} = useForm();
+    const { component } = usePage();
+
+    function submit(e) {
+        e.preventDefault();
+        destroy(`/posts/${post.id}`);
+    }
+    return (
+        <>
+            <Head title={component} />
+            <h1 className="text-3xl font-bold text-center">Post</h1>
+            <div className="font-medium border p-4 mt-4 rounded-lg bg-gray-50 shadow-lg">
+                <div className="flex justify-between">
+                    <div>
+                        <span className="font-bold">Posted: </span>
+                        <span className="text-sm font-thin">{new Date(post.created_at).toLocaleTimeString()}</span>
+                    </div>
+                    <div className="flex gap-2">
+                        <form onSubmit={submit}>
+                            <button disabled={processing}
+                                    className="text-sm text-white rounded-full p-1 bg-red-500 hover:bg-opacity-70 hover:shadow-md transition duration-300">Delete
+                            </button>
+                        </form>
+                        <Link href={`/posts/${post.id}/edit`}
+                              className="text-sm text-white rounded-full p-1 bg-green-500 hover:bg-opacity-70 hover:shadow-md transition duration-300"
+                        >Update
+                        </Link>
+                    </div>
+                </div>
+
+                <p>{post.body}</p>
+            </div>
+        </>
+    );
+}
