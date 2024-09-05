82b41c1b500e4b4945c4a5c37fb5ff73985cf0d0
Tenarien <mamateki7@gmail.com>
2024-09-05 19:42:27 +0100

    Added pagination, also a link to edit page and a dynamic head title using inertia

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,28 +1,31 @@
 import Layout from "@/Layouts/Layout.jsx";
-import {Link} from "@inertiajs/react";
+import {Head, Link, usePage} from "@inertiajs/react";
 
 export default function Home({ posts }) {
+    const { component } = usePage();
+
     return (
         <>
+            <Head title={component} />
             <h1 className="text-3xl">Hello</h1>
-
             <div>
                 {posts.data.map(post => (
                     <div key={post.id} className="p-4 border-b border-gray-200">
                         <span className="font-bold">Posted: </span>
-                        <span className="text-sm">{ new Date(post.created_at).toLocaleTimeString()}</span>
+                        <span className="text-sm">{new Date(post.created_at).toLocaleTimeString()}</span>
                         <p className="font-medium">{post.body}</p>
+                        <Link href={`/posts/${post.id}`} className="text-blue-500">Read more...</Link>
                     </div>
                 ))}
             </div>
             <div className="flex justify-center mt-4">
-                { posts.links.map(link => (
-                        link.url ?
+                {posts.links.map(link => (
+                    link.url ?
                         <Link
                             key={link.label}
                             href={link.url}
                             dangerouslySetInnerHTML={{__html: link.label}}
-                            className={`p-1 mx-1 ${ link.active ? "text-blue-500 font-bold" : ""
+                            className={`p-1 mx-1 ${link.active ? "text-blue-500 font-bold" : ""
                             }`}
                         />
                         :
