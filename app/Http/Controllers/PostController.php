fb555712e223882640cf2758a1a4e1705ba7ca47
Tenarien <mamateki7@gmail.com>
2024-09-05 19:43:35 +0100

    Implemented PostController with CRUD functionality using inertia and Laravel validation

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
-use App\Http\Requests\StorePostRequest;
-use App\Http\Requests\UpdatePostRequest;
+use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
@@ -23,15 +22,28 @@ class PostController extends Controller
      */
     public function create()
     {
-        //
+        return inertia('Create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StorePostRequest $request)
+    public function store(Request $request)
     {
-        //
+        sleep(2);
+        try {
+            $validatedData = $request->validate([
+                'body' => ['required', 'max:255', 'min:10']
+            ]);
+
+            Post::create($validatedData);
+
+            return redirect('/')->with('message', 'Post created!');
+        } catch (\Illuminate\Validation\ValidationException $e) {
+            return redirect()->back()->withErrors($e->errors())->with('error', 'Post creation failed!');
+        }
+
+
     }
 
     /**
@@ -39,7 +51,7 @@ class PostController extends Controller
      */
     public function show(Post $post)
     {
-        //
+        return inertia('Show', ['post' => $post]);
     }
 
     /**
@@ -47,15 +59,27 @@ class PostController extends Controller
      */
     public function edit(Post $post)
     {
-        //
+        return inertia('Edit', ['post' => $post]);
+
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdatePostRequest $request, Post $post)
+    public function update(Request $request, Post $post)
     {
-        //
+        try {
+            $validatedData = $request->validate([
+                'body' => ['required', 'max:255', 'min:10']
+            ]);
+
+            Post::updateOrCreate($validatedData);
+
+            return to_route('post.index')->with('success', 'Post updated successfully.');
+        } catch (\Illuminate\Validation\ValidationException $e) {
+            return back()->withErrors($e->errors())->with('error', 'Post update failed!');
+        }
+
     }
 
     /**
@@ -63,6 +87,8 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
-        //
+        $post->delete();
+
+        return redirect('/')->with('success', 'Post deleted!');
     }
 }
