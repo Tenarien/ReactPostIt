adbd99fd44cd71758d05098f55939855d1001732
Tenarien <mamateki7@gmail.com>
2024-09-10 00:49:46 +0100

    Created a route to post comments

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PostController::class, 'index'])->name('post.index');
 
 Route::resource('posts', PostController::class)->except('index');
+Route::post('/posts/{post}/comments', [CommentController::class, 'store']);
