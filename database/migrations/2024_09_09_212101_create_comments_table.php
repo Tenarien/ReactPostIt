2bb360379a75f2efda34680d6edc6ee7b2639787
Tenarien <mamateki7@gmail.com>
2024-09-10 19:37:22 +0100

    Added a parent_id column, constrained to comments table

--- database/migrations/2024_09_09_212101_create_comments_table.php
+++ database/migrations/2024_09_09_212101_create_comments_table.php
@@ -13,7 +13,8 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('post_id')->constrained('posts');
+            $table->foreignId('post_id')->constrained('posts')->cascadeOnDelete();
+            $table->foreignId('parent_id')->nullable()->constrained('comments')->cascadeOnDelete();
             $table->text('body');
             $table->timestamps();
         });
