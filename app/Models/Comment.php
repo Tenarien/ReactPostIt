a957c12d1013ba94234d340e36dff2e057386cf3
Tenarien <mamateki7@gmail.com>
2024-09-10 23:16:54 +0100

    Modified model for comments, added parent and replies relationships, also created business logic static methods to fetch replies, and parent comments

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Comment extends Model
 {
@@ -11,19 +13,46 @@ class Comment extends Model
 
     protected $table = 'comments';
     protected $fillable = [
-      'id',
-      'post_id',
-      'user_id',
-      'body',
+        'id',
+        'post_id',
+        'user_id',
+        'parent_id',
+        'body',
     ];
 
-    public function post()
+    public function post() :BelongsTo
     {
         return $this->belongsTo(Post::class);
     }
 
-    public function user()
+    public function user() :BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function parent(): BelongsTo
+    {
+        return $this->belongsTo(Comment::class, 'parent_id');
+    }
+
+    public function replies(): HasMany
+    {
+        return $this->hasMany(Comment::class, 'parent_id')->with('replies');
+    }
+
+    public static function getReplies($postId)
+    {
+        return self::where('post_id', $postId)
+            ->whereNotNull('parent_id')
+            ->with('replies')
+            ->get();
+    }
+
+    public static function getParentComments($postId)
+    {
+        return self::where('post_id', $postId)
+            ->whereNull('parent_id')
+            ->with('replies')
+            ->get();
     }
 }
