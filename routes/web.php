6185b1944ce7069de79e85c9d6b420b2614784eb
Tenarien <mamateki7@gmail.com>
2024-09-11 23:23:24 +0100

    Created routes for login, register, and logout. Also organized them in middleware(in progress)

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,30 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PostController::class, 'index'])->name('post.index');
-
 Route::resource('posts', PostController::class)->except('index');
-Route::post('/posts/{post}/comments', [CommentController::class, 'store']);
+
+Route::middleware('guest')->group(function () {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::get('/register', [AuthController::class, 'registerForm']);
+    Route::post('/register', [AuthController::class, 'register']);
+});
+
+
+Route::middleware('auth')->group(function () {
+    Route::post('/posts', [PostController::class, 'store']);
+    Route::put('/posts/{post}', [PostController::class, 'update']);
+    Route::delete('/posts/{post}', [PostController::class, 'destroy']);
+    Route::post('/posts/{post}/comments', [CommentController::class, 'store']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
+
+
+
+
+
+
