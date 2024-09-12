a02606f03075fe022141517c96d16e74386a654c
Tenarien <mamateki7@gmail.com>
2024-09-12 20:20:47 +0100

    Modify tables, to have user_id columns

--- database/migrations/2024_09_09_212101_create_comments_table.php
+++ database/migrations/2024_09_09_212101_create_comments_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->foreignId('post_id')->constrained('posts')->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->foreignId('parent_id')->nullable()->constrained('comments')->cascadeOnDelete();
             $table->text('body');
             $table->timestamps();
