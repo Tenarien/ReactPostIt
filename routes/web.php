ca9c5a1bb07e3fb4df54198e5bfbf5fc2eb6561b
Tenarien <mamateki7@gmail.com>
2024-09-12 20:31:42 +0100

    Added some routes, and modified the order and middleware of them

--- routes/web.php
+++ routes/web.php
@@ -7,21 +7,24 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PostController::class, 'index'])->name('post.index');
 Route::resource('posts', PostController::class)->except('index');
+Route::get('/post/{post}', [PostController::class, 'show']);
 
 Route::middleware('guest')->group(function () {
+    Route::get('/register', [AuthController::class, 'registerForm'])->name('register.form');
+    Route::post('/register', [AuthController::class, 'register'])->name('register');
     Route::post('/login', [AuthController::class, 'login']);
-    Route::get('/register', [AuthController::class, 'registerForm']);
-    Route::post('/register', [AuthController::class, 'register']);
+    Route::get('/login', [AuthController::class, 'loginForm'])->name('login');
 });
-
 
 Route::middleware('auth')->group(function () {
-    Route::post('/posts', [PostController::class, 'store']);
-    Route::put('/posts/{post}', [PostController::class, 'update']);
-    Route::delete('/posts/{post}', [PostController::class, 'destroy']);
-    Route::post('/posts/{post}/comments', [CommentController::class, 'store']);
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/posts/create', [PostController::class, 'create']);
+    Route::post('/posts/{post}', [PostController::class, 'update']);
+    Route::post('/posts/{post}', [PostController::class, 'destroy']);
+    Route::post('/posts/{post}', [PostController::class, 'store']);
+    Route::post('/posts/{post}/comments', [CommentController::class, 'store']);
 });
+
 
 
 
