9bd4d5d2abc5d72e3fd8705385955d1425c9ddca
Tenarien <mamateki7@gmail.com>
2024-09-13 20:40:41 +0100

    Added belongs to user relationship

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Post extends Model
 {
@@ -16,8 +18,13 @@ class Post extends Model
         'user_id',
     ];
 
-    public function comments()
+    public function comments() :HasMany
     {
         return $this->hasMany(Comment::class);
+    }
+
+    public function user() :BelongsTo
+    {
+        return $this->belongsTo(User::class);
     }
 }
