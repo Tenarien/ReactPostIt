82bbda13658ec0f16fbafd57f9d7aa596777af8c
Tenarien <mamateki7@gmail.com>
2024-09-13 20:36:55 +0100

    Moved logic to post a comment from Show to commentForm, and changed the design of the commentForm

--- resources/js/Components/Comments/CommentForm.jsx
+++ resources/js/Components/Comments/CommentForm.jsx
@@ -1,14 +1,22 @@
-import React, {useRef, useState} from 'react';
+import React, { useState} from 'react';
+import {useForm, usePage} from "@inertiajs/react";
 
-function CommentForm({ onSubmit, value, onChange, processing, errors }) {
-    const textareaRef = useRef();
+function CommentForm({ post }) {
     const [showSubmit, setShowSubmit] = useState(false);
 
-    const handleSubmit = (e) => {
+    const { data, setData, post: submitComment, processing: processing, errors, reset } = useForm({
+        body: '',
+        post_id: `${post.id}`,
+        user_id: usePage().props.auth.user ? usePage().props.auth.user.id : undefined,
+    });
+
+    function handleCommentSubmit(e) {
         e.preventDefault();
-        onSubmit(e);
-        textareaRef.current.value = '';
-    };
+        submitComment(`/posts/${post.id}/comments`, {
+            onSuccess: () => reset(),
+            onError: () => console.error('Comment submission failed'),
+        });
+    }
 
     const handleFocus = () => {
         setShowSubmit(true);
@@ -19,24 +27,34 @@ function CommentForm({ onSubmit, value, onChange, processing, errors }) {
     };
 
     return (
-        <form onSubmit={handleSubmit} className="mt-4">
-                    <textarea
-                        ref={textareaRef}
-                        className="w-full border rounded-lg p-2"
-                        placeholder="Add a comment..."
-                        rows="1"
-                        value={value}
-                        onChange={onChange}
-                        onFocus={handleFocus}
-                        onBlur={handleBlur}
-                    >
-                    </textarea>
-            {errors.body && <p className="text-sm text-opacity-70 text-red-600 font-semibold">{ errors.body }</p>}
-            {showSubmit && (<button type="submit" disabled={processing}
-                                    className="mt-2 bg-blue-500 text-white p-2 rounded-lg hover:bg-blue-600 transition"
-            >{processing ? 'Commenting...' : 'Comment'}
-            </button>)}
-        </form>
+        <>
+            <form onSubmit={handleCommentSubmit}
+                  className={`relative ${showSubmit ? 'pb-14' : ''} mt-4 bg-gray-50 p-4 border border-gray-200 rounded-lg shadow-sm`}>
+                <textarea
+                    className="w-full p-3 border border-gray-300 rounded-lg focus:border-orange-500 focus:ring-1 focus:ring-orange-500"
+                    placeholder="Add a comment..."
+                    rows="2"
+                    value={data.body}
+                    onChange={e => setData('body', e.target.value)}
+                    onFocus={handleFocus}
+                    onBlur={handleBlur}
+                />
+                {errors.body && (
+                    <p className="text-sm text-red-600 font-semibold mt-1">{errors.body}</p>
+                )}
+                {showSubmit && (
+                    <div className="absolute bottom-3 right-4">
+                        <button
+                            type="submit"
+                            disabled={processing}
+                            className="px-4 py-2 bg-orange-500 text-white rounded-lg hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-orange-400 transition duration-300 ease-in-out"
+                        >
+                            {processing ? 'Commenting...' : 'Comment'}
+                        </button>
+                    </div>
+                )}
+            </form>
+        </>
     );
 }
 
