3712f3656d55d69af33af82079be6524e4f7ee5b
Tenarien <mamateki7@gmail.com>
2024-09-13 20:37:40 +0100

    Removed function to post a comment from Show and moved it to commentForm, also changed the design of the page

--- resources/js/Pages/Show.jsx
+++ resources/js/Pages/Show.jsx
@@ -1,66 +1,66 @@
 import {Head, Link, useForm, usePage} from "@inertiajs/react";
 import { formatDistanceToNow } from 'date-fns'
 import CommentsSection from "@/Components/Comments/CommentsSection.jsx";
+import {useEffect, useState} from "react";
 
-export default function Show({ post, comments, replies }) {
+export default function Show({ post, comments }) {
     const {delete: destroy, processing} = useForm();
     const { component } = usePage();
+    const { props } = usePage();
+    const { auth } = props;
 
-    const { data, setData, post: submitComment, processing: commentProcessing, errors } = useForm({
-        body: '',
-        post_id: `${post.id}`,
-        user_id: usePage().props.auth.user.id,
-    });
-
+    const [showPostOptions, setShowPostOptions] = useState(false);
 
     function handlePostDeletion(e) {
         e.preventDefault();
         destroy(`/posts/${post.id}`);
     }
 
-    function handleCommentSubmit(e) {
-        e.preventDefault();
-        submitComment(`/posts/${post.id}/comments`, {
-            onError: () => console.error('Comment submission failed'),
-        });
-    }
     return (
         <>
             <Head title={component} />
-            <h1 className="text-3xl font-bold text-center">Post</h1>
-            <div className="font-medium border p-4 mt-4 rounded-lg bg-gray-50 shadow-lg">
-                <div className="flex justify-between">
+            <div className="relative font-medium border p-4 mt-4 rounded-lg bg-gray-50 shadow-lg">
+                {auth.user && post.user.id === auth.user.id && (<button
+                    onClick={() => setShowPostOptions(!showPostOptions)}
+                    className={`
+                    absolute font-bold top-0 h-6 w-6 right-0 flex
+                    items-center justify-center rounded hover:bg-black hover:bg-opacity-25 active:ring-2 ring-black
+                    ${showPostOptions ? 'bg-black bg-opacity-25' : ''}
+                    `}
+                >...
+                </button>)}
+                {/* Post Header */}
+                <div className="flex justify-between items-center mb-4">
                     <div>
-                        <span className="text-black font-bold text-xl mr-4">{ usePage().props.auth.user.name}</span>
-                        <span className="font-semibold">Posted: </span>
-                        <span className="text-sm font-thin">{formatDistanceToNow(new Date(post.created_at), { addSuffix: true })}</span>
+                        <span className="text-xl font-bold text-gray-800 mr-4">{post.user.name}</span>
+                        <span className="text-sm text-gray-500 font-light">
+                            {formatDistanceToNow(new Date(post.created_at), {addSuffix: true})}
+                        </span>
                     </div>
-                    <div className="flex gap-2">
-                        <form onSubmit={handlePostDeletion}>
-                            <button disabled={processing}
-                                    className="text-sm text-white rounded-full p-1 bg-red-500 hover:bg-opacity-70 hover:shadow-md transition duration-300">Delete
-                            </button>
-                        </form>
-                        <Link href={`/posts/${post.id}/edit`}
-                              className="text-sm text-white rounded-full p-1 bg-green-500 hover:bg-opacity-70 hover:shadow-md transition duration-300"
-                        >Update
-                        </Link>
+                    <div
+                        className={`relative transition-all duration-300 ease-in-out ${showPostOptions ? `opacity-100` : `opacity-0`}`}>
+                        {showPostOptions && post.user.id === auth.user.id && (<div
+                            className={`absolute bg-gray-200 p-3 rounded-lg border border-gray-400 flex right-5 flex-col gap-2 ${showPostOptions ? `block` : `hidden`}`}>
+                            <form onSubmit={handlePostDeletion}>
+                                <button disabled={processing}
+                                        className="text-sm text-white w-full rounded-full p-1 bg-red-500 hover:bg-opacity-70 hover:shadow-md transition duration-300">Delete
+                                </button>
+                            </form>
+                            <Link href={`/posts/${post.id}/edit`}
+                                  className="text-sm w-full text-white rounded-full p-1 bg-green-500 hover:bg-opacity-70 hover:shadow-md transition duration-300"
+                            >Update
+                            </Link>
+                        </div>)}
                     </div>
                 </div>
 
-                <p>{post.body}</p>
+                <p className="text-gray-800">{post.body}</p>
             </div>
 
             {/* Comment Section */}
             <CommentsSection
                 post={post}
-                parentComments={comments}
-                replies={replies}
-                onCommentSubmit={handleCommentSubmit}
-                commentData={data}
-                setCommentData={setData}
-                processing={commentProcessing}
-                errors={errors}
+                comments={comments}
             />
         </>
     );
