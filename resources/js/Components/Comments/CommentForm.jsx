f1a6ed362470acbfb19fb0b5d3e792912dcb9d16
Tenarien <mamateki7@gmail.com>
2024-09-16 20:35:33 +0100

    Slight re-design and added a way to cancel the comment form

--- resources/js/Components/Comments/CommentForm.jsx
+++ resources/js/Components/Comments/CommentForm.jsx
@@ -13,7 +13,10 @@ function CommentForm({ post }) {
     function handleCommentSubmit(e) {
         e.preventDefault();
         submitComment(`/posts/${post.id}/comments`, {
-            onSuccess: () => reset(),
+            onSuccess: () => {
+              reset();
+              setShowSubmit(false);
+            },
             onError: () => console.error('Comment submission failed'),
         });
     }
@@ -22,14 +25,15 @@ function CommentForm({ post }) {
         setShowSubmit(true);
     };
 
-    const handleBlur = () => {
+    const handleCancel = () => {
         setTimeout(() => setShowSubmit(false), 200);
+        reset();
     };
 
     return (
         <>
             <form onSubmit={handleCommentSubmit}
-                  className={`relative ${showSubmit ? 'pb-14' : ''} mt-4 bg-gray-50 p-4 border border-gray-200 rounded-lg shadow-sm`}>
+                  className={`relative ${showSubmit ? 'pb-14' : ''} mt-4`}>
                 <textarea
                     className="w-full p-3 border border-gray-300 rounded-lg focus:border-orange-500 focus:ring-1 focus:ring-orange-500"
                     placeholder="Add a comment..."
@@ -37,17 +41,28 @@ function CommentForm({ post }) {
                     value={data.body}
                     onChange={e => setData('body', e.target.value)}
                     onFocus={handleFocus}
-                    onBlur={handleBlur}
                 />
                 {errors.body && (
                     <p className="text-sm text-red-600 font-semibold mt-1">{errors.body}</p>
                 )}
                 {showSubmit && (
-                    <div className="absolute bottom-3 right-4">
+                    <div className="absolute bottom-3 right-0 text-xs">
+                        <button
+                            type="reset"
+                            onClick={handleCancel}
+                            disabled={processing}
+                            className="px-4 py-2 mr-2 text-orange-500 border border-orange-500 rounded-lg hover:text-white hover:bg-orange-500 focus:outline-none focus:ring-2 focus:ring-orange-400 transition duration-300 ease-in-out"
+                        >
+                            Cancel
+                        </button>
                         <button
                             type="submit"
-                            disabled={processing}
-                            className="px-4 py-2 bg-orange-500 text-white rounded-lg hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-orange-400 transition duration-300 ease-in-out"
+                            disabled={processing || data.body.length < 10}
+                            className=
+                                {`px-4 py-2 text-white rounded-lg focus:outline-none focus:ring-2
+                                    focus:ring-orange-400 transition duration-300 ease-in-out
+                                    ${data.body.length >= 10 ? 'bg-orange-500 hover:bg-orange-700' : 'bg-gray-500'}`
+                                }
                         >
                             {processing ? 'Commenting...' : 'Comment'}
                         </button>
