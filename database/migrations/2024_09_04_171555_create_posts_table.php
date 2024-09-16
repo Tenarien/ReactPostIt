39c553f8a414ff51f85c6305ff0c32182c5af8cd
Tenarien <mamateki7@gmail.com>
2024-09-16 20:52:55 +0100

    Fix typos

--- database/migrations/2024_09_04_171555_create_posts_table.php
+++ database/migrations/2024_09_04_171555_create_posts_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
             $table->text('body');
             $table->timestamps();
         });
--- database/migrations/2024_09_09_212101_create_comments_table.php
+++ database/migrations/2024_09_09_212101_create_comments_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->foreignId('post_id')->constrained('posts')->cascadeOnDelete();
-            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
             $table->foreignId('parent_id')->nullable()->constrained('comments')->cascadeOnDelete();
             $table->text('body');
             $table->timestamps();
