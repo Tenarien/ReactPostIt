b54b8a32fddd4da783114aa3b46b8b962f46a693
Tenarien <mamateki7@gmail.com>
2024-09-16 20:44:51 +0100

    Added a many-to-many relationship, between post_id and user_id through the likes table

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Post extends Model
@@ -26,5 +27,10 @@ class Post extends Model
     public function user() :BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function likers() :BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'likes', 'post_id', 'user_id')->withTimestamps();
     }
 }
