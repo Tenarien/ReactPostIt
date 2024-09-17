2d72e4bc6b3dc3c4d9a5f3aaab93cb7ad4393570
Tenarien <mamateki7@gmail.com>
2024-09-17 16:41:36 +0100

    Added routes, to fetch and like comments, and also renamed routes to be better aligned with what they are supposed to do

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [PostController::class, 'index'])->name('post.index');
 Route::resource('posts', PostController::class)->except('index');
 Route::get('/post/{post}', [PostController::class, 'show']);
-Route::get('/comments/{id}/replies', [CommentController::class, 'fetchReplies']);
+Route::get('/comments/{comment}/replies', [CommentController::class, 'fetchReplies']);
+Route::get('/comments/{comment}/likes', [CommentController::class, 'fetchCommentLikes']);
 
 Route::middleware('guest')->group(function () {
     Route::get('/register', [AuthController::class, 'registerForm'])->name('register.form');
@@ -19,11 +20,15 @@ Route::middleware('guest')->group(function () {
 
 Route::middleware('auth')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+
     Route::get('/posts/create', [PostController::class, 'create']);
     Route::post('/posts/{post}', [PostController::class, 'update']);
     Route::post('/posts/{post}', [PostController::class, 'destroy']);
     Route::post('/posts/{post}', [PostController::class, 'store']);
     Route::post('/posts/{post}/comments', [CommentController::class, 'store']);
+
+    Route::post('/posts/{post}/like', [PostController::class, 'like']);
+    Route::post('/comments/{comment}/like', [CommentController::class, 'like']);
 });
 
 
