c9067990b973833d3f2ab9bfcc6eb081866cc101
Tenarien <mamateki7@gmail.com>
2024-09-17 17:40:41 +0100

    Added a drop-down box, to edit and delete post, also added an edit component so that you can edit the post from the show page, and a PostReactions component, to like a post and see its likes. Along with a design change to align with the website theme

--- resources/js/Pages/Show.jsx
+++ resources/js/Pages/Show.jsx
@@ -1,19 +1,47 @@
 import {Head, Link, useForm, usePage} from "@inertiajs/react";
 import { formatDistanceToNow } from 'date-fns'
+import {useEffect, useRef, useState} from "react";
 import CommentsSection from "@/Components/Comments/CommentsSection.jsx";
-import {useEffect, useState} from "react";
+import EditPostForm from "@/Components/Posts/EditPostForm.jsx";
+import PostReactions from "@/Components/Posts/PostReactions.jsx";
 
-export default function Show({ post, comments }) {
-    const {delete: destroy, processing} = useForm();
-    const { component } = usePage();
-    const { props } = usePage();
+export default function Show({ post, comments, hasLikedPost, postLikes, }) {
+    const { props, component } = usePage();
     const { auth } = props;
+    const {delete: destroy, processing} = useForm({
+        user_id: props.auth.user ? props.auth.user.id : undefined,
+        body: post.body,
+    });
 
     const [showPostOptions, setShowPostOptions] = useState(false);
+    const [editMode, setEditMode] = useState(false);
+
+    const optionsRef = useRef(null);
+    const buttonRef = useRef(null);
+
+    useEffect(() => {
+        function handleClickOutside(e) {
+            if (optionsRef.current && !optionsRef.current.contains(e.target) &&
+                buttonRef.current && !buttonRef.current.contains(e.target)) {
+                setShowPostOptions(false);
+            }
+        }
+
+        document.addEventListener("click", handleClickOutside);
+
+        return () => {
+            document.removeEventListener("click", handleClickOutside);
+        };
+
+    }, [setShowPostOptions]);
 
     function handlePostDeletion(e) {
         e.preventDefault();
         destroy(`/posts/${post.id}`);
+    }
+
+    function handleEditComplete() {
+        setEditMode(false);
     }
 
     return (
@@ -21,11 +49,12 @@ export default function Show({ post, comments }) {
             <Head title={component} />
             <div className="relative font-medium border p-4 mt-4 rounded-lg bg-gray-50 shadow-lg">
                 {auth.user && post.user.id === auth.user.id && (<button
+                    ref={buttonRef}
                     onClick={() => setShowPostOptions(!showPostOptions)}
                     className={`
                     absolute font-bold top-0 h-6 w-6 right-0 flex
-                    items-center justify-center rounded hover:bg-black hover:bg-opacity-25 active:ring-2 ring-black
-                    ${showPostOptions ? 'bg-black bg-opacity-25' : ''}
+                    items-center justify-center rounded hover:bg-orange-500 hover:bg-opacity-25 active:ring-2 ring-black
+                    ${showPostOptions ? 'bg-orange-500 bg-opacity-50' : ''}
                     `}
                 >...
                 </button>)}
@@ -37,25 +66,50 @@ export default function Show({ post, comments }) {
                             {formatDistanceToNow(new Date(post.created_at), {addSuffix: true})}
                         </span>
                     </div>
+                    {/* Options */}
                     <div
-                        className={`relative transition-all duration-300 ease-in-out ${showPostOptions ? `opacity-100` : `opacity-0`}`}>
+                        ref={optionsRef}
+                        className={`relative z-40 transition-all duration-500 ease-in-out ${showPostOptions ? `opacity-100` : `opacity-0`}`}>
                         {showPostOptions && post.user.id === auth.user.id && (<div
-                            className={`absolute bg-gray-200 p-3 rounded-lg border border-gray-400 flex right-5 flex-col gap-2 ${showPostOptions ? `block` : `hidden`}`}>
+                            className={`absolute text-center bg-gray-100 w-20 py-4 shadow-lg rounded border border-orange-500 flex right-5 flex-col gap-2
+                            ${showPostOptions ? `block` : `hidden`}`}>
                             <form onSubmit={handlePostDeletion}>
                                 <button disabled={processing}
-                                        className="text-sm text-white w-full rounded-full p-1 bg-red-500 hover:bg-opacity-70 hover:shadow-md transition duration-300">Delete
+                                        className="text-sm text-orange-500 w-full p-1  border-orange-500 hover:bg-red-500 hover:text-white hover:shadow-md transition duration-300">
+                                    {processing ? 'Deleting...' : 'Delete'}
                                 </button>
                             </form>
-                            <Link href={`/posts/${post.id}/edit`}
-                                  className="text-sm w-full text-white rounded-full p-1 bg-green-500 hover:bg-opacity-70 hover:shadow-md transition duration-300"
+                            <button
+                                onClick={() => {
+                                    setEditMode(!editMode);
+                                    setShowPostOptions(!showPostOptions);
+                                }}
+                                className="text-sm text-orange-500 w-full p-1  border-orange-500 hover:bg-green-500 hover:text-white hover:shadow-md transition-all duration-300"
                             >Update
-                            </Link>
+                            </button>
                         </div>)}
                     </div>
                 </div>
 
-                <p className="text-gray-800">{post.body}</p>
+                {editMode
+                    ?
+                    (<EditPostForm
+                        post={post}
+                        onEditComplete={handleEditComplete}
+                    />)
+                    :
+                    (<p className="text-gray-800">{post.body}</p>)
+                }
+
+                <div className="mt-4">
+                    <PostReactions
+                        post={post}
+                        hasLikedPost={hasLikedPost}
+                        postLikes={postLikes}
+                    />
+                </div>
             </div>
+
 
             {/* Comment Section */}
             <CommentsSection
