05cc64c5747fa18491fc60b0c65ba7455df7f71e
Tenarien <mamateki7@gmail.com>
2024-09-23 21:19:35 +0100

    Renamed the component, and changed its post request behaviour to return some props back to make state changes

--- resources/js/Components/Comments/CreateCommentForm.jsx
+++ resources/js/Components/Comments/CreateCommentForm.jsx
@@ -1,23 +1,30 @@
 import React, { useState} from 'react';
 import {useForm, usePage} from "@inertiajs/react";
 
-function CommentForm({ post }) {
+function CreateCommentForm({ post, comment, addComment, onReplyAdded }) {
     const [showSubmit, setShowSubmit] = useState(false);
 
     const { data, setData, post: submitComment, processing: processing, errors, reset } = useForm({
         body: '',
         post_id: post.id,
         user_id: usePage().props.auth.user ? usePage().props.auth.user.id : undefined,
+        parent_id: comment ? comment.id : undefined,
     });
 
     function handleCommentSubmit(e) {
         e.preventDefault();
-        submitComment(`/posts/${post.id}/comments`, {
-            onSuccess: () => {
-              reset();
-              setShowSubmit(false);
+
+        const url = `/posts/${post.id}/comments`;
+
+        submitComment(url, {
+            onSuccess: (response) => {
+                const newComment = response.props.comments[response.props.comments.length - 1];
+                addComment(newComment.parent_id, newComment);
+                reset();
+                setShowSubmit(false);
+                if (onReplyAdded) onReplyAdded();
             },
-            onError: () => console.error('Comment submission failed'),
+            onError: () => console.error('CommentItem submission failed'),
         });
     }
 
@@ -25,10 +32,6 @@ function CommentForm({ post }) {
         setShowSubmit(true);
     };
 
-    const handleCancel = () => {
-        setTimeout(() => setShowSubmit(false), 200);
-        reset();
-    };
 
     return (
         <>
@@ -48,20 +51,12 @@ function CommentForm({ post }) {
                 {showSubmit && (
                     <div className="absolute bottom-3 right-0 text-xs">
                         <button
-                            type="reset"
-                            onClick={handleCancel}
-                            disabled={processing}
-                            className="px-4 py-2 mr-2 text-orange-500 border border-orange-500 rounded-lg hover:text-white hover:bg-orange-500 focus:outline-none focus:ring-2 focus:ring-orange-400 transition duration-300 ease-in-out"
-                        >
-                            Cancel
-                        </button>
-                        <button
                             type="submit"
-                            disabled={processing || data.body.length < 10}
+                            disabled={processing || data.body.length < 1}
                             className=
                                 {`px-4 py-2 text-white rounded-lg focus:outline-none focus:ring-2
                                     focus:ring-orange-400 transition duration-300 ease-in-out
-                                    ${data.body.length >= 10 ? 'bg-orange-500 hover:bg-orange-700' : 'bg-gray-500'}`
+                                    ${data.body.length >= 1 ? 'bg-orange-500 hover:bg-orange-700' : 'bg-gray-500'}`
                                 }
                         >
                             {processing ? 'Commenting...' : 'Comment'}
@@ -73,4 +68,4 @@ function CommentForm({ post }) {
     );
 }
 
-export default CommentForm;
+export default CreateCommentForm;
