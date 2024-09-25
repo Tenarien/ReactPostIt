4e19bd5a77033402d4d9d8c2ed9023f16ca59abd
Tenarien <mamateki7@gmail.com>
2024-09-25 21:50:02 +0100

    Added Avatar component to the Layout

--- resources/js/Layouts/Layout.jsx
+++ resources/js/Layouts/Layout.jsx
@@ -1,25 +1,27 @@
 import {Link, usePage} from '@inertiajs/react'
+import Avatar from "@/Components/Auth/Avatar.jsx"
 import Flash from "@/Layouts/Flash.jsx";
 import LoginForm from "@/Components/Auth/LoginForm.jsx";
 
 export default function Layout({ children }) {
     const { props } = usePage();
     const { auth } = props;
-    const message = props.flash.message;
 
     return (
         <>
             <header className="sticky w-full top-0 z-50 py-2 px-10 bg-zinc-600 text-white shadow-lg">
                 <nav className="flex items-center justify-between font-bold text-lg">
                     <Link className="p-2 hover:bg-zinc-400 hover:bg-opacity-50 rounded-lg" href="/">Home</Link>
+
                     {!auth.user ? (
                         <LoginForm />
-                    ) : (<Link
-                        href={`/logout`}
-                        method="post"
-                        as="button"
-                        className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded"
-                    >Logout</Link>)}
+                    ) : (
+                        <div>
+                            <Avatar
+                                user={auth.user ? auth.user.name : null}
+                            />
+                        </div>)
+                        }
 
                 </nav>
             </header>
