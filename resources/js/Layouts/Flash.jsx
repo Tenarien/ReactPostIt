bcd16d9215e6182e954aa2b2af32c3dcf0b7ac5b
Tenarien <mamateki7@gmail.com>
2024-09-26 21:15:41 +0100

    Refactored the animation to use framer-motion instead of controlling the state using js, and added useEffect to update flash message on url change to fix a bug that was causing the message to stay on the screen

--- resources/js/Layouts/Flash.jsx
+++ resources/js/Layouts/Flash.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 import { usePage } from "@inertiajs/react";
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function Flash() {
     const { flash } = usePage().props;
+    const { url } = usePage()
     const [visible, setVisible] = useState(false);
     const [currentMessage, setCurrentMessage] = useState('');
     const [messageType, setMessageType] = useState('');
@@ -29,6 +31,13 @@ export default function Flash() {
         }
     }, [flash]);
 
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setVisible(false);
+        }, 3000);
+        return () => clearTimeout(timer);
+    }, [url]);
+
 
     const getBorderColor = () => {
         switch (messageType) {
@@ -40,14 +49,18 @@ export default function Flash() {
     };
 
     return (
-        <div
-            className={`fixed z-50 w-fit bottom-4 right-4 px-4 py-2 m-4 border
-             rounded-lg bg-white text-orange-500 transform duration-300 transition-all ease-in-out
-
-             ${getBorderColor()}
-             ${visible ? 'translate-y-0 opacity-100' : 'translate-y-full opacity-0'}`}
-        >
-            <p className="font-bold">{currentMessage}</p>
-        </div>
+        <AnimatePresence>
+            {visible && (
+                <motion.div
+                    className={`fixed z-50 w-fit bottom-4 right-4 px-4 py-2 m-4 border rounded-lg bg-white text-orange-500 ${getBorderColor()}`}
+                    initial={{ opacity: 0, y: 50 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    exit={{ opacity: 0, y: 50 }}
+                    transition={{ duration: 0.5, ease: "easeInOut" }}
+                >
+                    <p className="font-bold">{currentMessage}</p>
+                </motion.div>
+            )}
+        </AnimatePresence>
     );
 }
