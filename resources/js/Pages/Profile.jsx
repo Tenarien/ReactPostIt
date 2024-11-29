04ceec113f9bdd36d6cda73a13961461773b99b5
Tenarien <mamateki7@gmail.com>
2024-11-29 19:17:05 +0000

    Some design change, and imported FollowSection and UserPosts components and passed appropriate props to them

--- resources/js/Pages/Profile.jsx
+++ resources/js/Pages/Profile.jsx
@@ -1,11 +1,13 @@
 import React, {useState} from "react";
 import {Head, usePage} from "@inertiajs/react";
-import { motion } from "framer-motion";
+import {motion} from "framer-motion";
 import EditProfileForm from "@/Components/Auth/EditProfileForm.jsx";
+import UserPosts from "@/Components/Posts/UserPosts.jsx";
+import FollowSection from "@/Components/Auth/FollowSection.jsx";
 
-export default function Profile({ user }) {
-    const { props } = usePage();
-    const { auth } = props;
+export default function Profile({user, posts}) {
+    const {props} = usePage();
+    const {auth} = props;
     const [isEditing, setIsEditing] = useState(false);
     const isCurrentUser = auth.user ? auth.user.id === user.id : false;
     const username = user.name.substring(0, 1).toUpperCase();
@@ -17,28 +19,35 @@ export default function Profile({ user }) {
     return (
         <>
             <Head title={user.name}/>
-            <div className="flex justify-center items-center">
-                <div className="bg-white shadow-md overflow-hidden w-full max-w-4xl">
+            <div className="flex flex-col justify-center items-center">
+                <div className="bg-white shadow-md overflow-hidden w-full">
                     {/* Header */}
-                    <div className="relative bg-orange-500 p-8 pb-20 text-white text-center rounded-t-lg">
-                        <div className="relative w-24 h-24 mx-auto mb-4 border-4 border-white rounded-full ">
-                            <div className="flex justify-center items-center bg-white w-full h-full rounded-full border-4 border-orange-500">
-                                <p className="text-3xl font-bold text-orange-500">{username}</p>
+                    <div className="flex flex-col justify-between">
+                        <div className="relative bg-orange-500 p-8 pb-20 text-white text-center rounded-t-lg">
+                            <div className="relative w-24 h-24 mx-auto mb-4 border-4 border-white rounded-full ">
+                                <div
+                                    className="flex justify-center items-center bg-white w-full h-full rounded-full border-4 border-orange-500">
+                                    <p className="text-3xl font-bold text-orange-500">{username}</p>
+                                </div>
                             </div>
+                            <h1 className="text-3xl font-bold">{user.name || "John Doe"}</h1>
                         </div>
-                        <h1 className="text-3xl font-bold">{user.name || "John Doe"}</h1>
+                        <div className="relative bg-orange-500 text-white p-6 flex">
+                            <FollowSection user={user}/>
 
-                        {/* Button to Trigger Form (Visible only if the current user is viewing their own profile) */}
-                        {isCurrentUser && (
-                            <button
-                                onClick={() => setIsEditing(!isEditing)}
-                                className="absolute right-4 bottom-4 bg-white text-orange-500 px-4 py-2 mt-4
+                            {/* Button to Trigger Form (Visible only if the current user is viewing their own profile) */}
+                            {isCurrentUser && (
+                                <button
+                                    onClick={() => setIsEditing(!isEditing)}
+                                    className="absolute right-4 bottom-4 bg-white text-orange-500 px-4 py-2 mt-4
                                     rounded-md shadow hover:bg-orange-600 hover:text-white transition-all duration-300"
-                            >
-                                {isEditing ? "Close Edit Profile" : "Edit Profile"}
-                            </button>
-                        )}
+                                >
+                                    {isEditing ? "Close Edit Profile" : "Edit Profile"}
+                                </button>
+                            )}
+                        </div>
                     </div>
+
 
                     {/* Body */}
                     <div className="p-8 space-y-8 rounded-b-lg border border-orange-500">
@@ -58,6 +67,9 @@ export default function Profile({ user }) {
                         />
                     </div>
                 </div>
+                <UserPosts
+                    posts={posts}
+                />
             </div>
         </>
     );
