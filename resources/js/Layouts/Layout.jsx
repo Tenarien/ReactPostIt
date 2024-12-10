c99e75a4e98735db264ba3a721ec5991123142c9
Tenarien <mamateki7@gmail.com>
2024-12-10 21:01:11 +0000

    Added connection using laravel echo, also added state for notifications and passed it into NotificationsDropdown component

--- resources/js/Layouts/Layout.jsx
+++ resources/js/Layouts/Layout.jsx
@@ -2,10 +2,24 @@ import {Link, usePage} from '@inertiajs/react'
 import Avatar from "@/Components/Auth/Avatar.jsx"
 import Flash from "@/Layouts/Flash.jsx";
 import LoginForm from "@/Components/Auth/LoginForm.jsx";
+import NotificationsDropdown from "@/Components/Auth/NotificationsDropdown.jsx";
+import {useEffect, useState} from "react";
 
 export default function Layout({ children }) {
     const { props } = usePage();
+    const [notifications, setNotifications] = useState(props.notifications || []);
     const { auth } = props;
+
+    useEffect(() => {
+        if(!auth.user) return;
+
+        Echo.private(`user.notifications.${auth.user.id}`)
+            .listen("Notifications", (notification) => {
+                console.log("Notification received:", notification);
+                setNotifications((prev) => [notification, ...prev]);
+            })
+            .error((error) => console.error('Subscription error:', error));
+    }, []);
     return (
         <>
             <header className="sticky w-full top-0 z-50 py-2 px-10 bg-zinc-600 text-white shadow-lg">
@@ -21,7 +35,8 @@ export default function Layout({ children }) {
                     {!auth.user ? (
                         <LoginForm />
                     ) : (
-                        <div>
+                        <div className="flex gap-2 items-center">
+                            <NotificationsDropdown notifications={notifications}/>
                             <Avatar
                                 user={auth.user ? auth.user.name : null}
                             />
--- routes/channels.php
+++ routes/channels.php
@@ -1,9 +1,8 @@
 <?php
 
-use App\Models\Post;
 use App\Models\User;
 use Illuminate\Support\Facades\Broadcast;
 
-Broadcast::channel('user.notifications.{id}', function ($user, $id) {
+Broadcast::channel('user.notifications.{id}', function (User $user, $id) {
     return (int) $user->id === (int) $id;
 });
