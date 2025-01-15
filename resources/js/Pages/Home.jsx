5b93a33c01ee50c350b59755207f116c5bde4173
Tenarien <mamateki7@gmail.com>
2025-01-15 18:31:54 +0000

    Removed logic, that was displaying posts and loading more posts and moved it to UserPosts component

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,16 +1,13 @@
 import CreatePostForm from "@/Components/Posts/CreatePostForm.jsx";
 import {Head, Link, router, usePage} from "@inertiajs/react";
-import { formatDistanceToNow } from "date-fns";
 import React, {useEffect, useState} from "react";
-import {SyncLoader} from "react-spinners";
 import { FaArrowLeft } from "react-icons/fa";
+import UserPosts from "@/Components/Posts/UserPosts.jsx";
 
 
 export default function Home({ posts }) {
     const { props } = usePage();
     const [postData, setPostData] = useState(posts);
-    const [loading, setLoading] = useState(false);
-    const [nextPage, setNextPage] = useState(postData.next_page_url);
 
     const addNewPost = (newPost) => {
         setPostData((prevData) => ({
@@ -18,44 +15,6 @@ export default function Home({ posts }) {
             data: [newPost, ...prevData.data],
         }));
     };
-
-    const loadMorePosts = async () => {
-        if (nextPage && !loading) {
-            setLoading(true);
-
-            router.get(nextPage, {}, {
-                preserveScroll: true,
-                preserveState: true,
-                preserveUrl: true,
-                replace: true,
-                only: ['posts'],
-
-                onSuccess: (page) => {
-                    setPostData({
-                        ...page.props.posts,
-                        data: [...postData.data, ...page.props.posts.data], // Append new posts
-                    });
-                    setNextPage(page.props.posts.next_page_url);
-                    setLoading(false);
-                },
-                onError: (error) => {
-                    console.error('Error loading more posts:', error);
-                    setLoading(false);
-                },
-            });
-        }
-    };
-
-    const handleScroll = () => {
-        if (window.innerHeight + document.documentElement.scrollTop + 50 >= document.documentElement.offsetHeight && !loading) {
-            loadMorePosts();
-        }
-    };
-
-    useEffect(() => {
-        window.addEventListener('scroll', handleScroll);
-        return () => window.removeEventListener('scroll', handleScroll); // Clean up event listener on component unmount
-    }, [nextPage, loading]);
 
     return (
         <>
@@ -77,40 +36,7 @@ export default function Home({ posts }) {
                         </div>
                     </div>
                 )}
-                {postData.data.map(post => (
-                    <div key={post.id} className="p-4 border rounded-lg shadow-md bg-white border-gray-200">
-                        {/* Post Header */}
-                        <div className="flex justify-between items-center mb-2">
-                            <Link
-                                href={`/profile/${post.user.id}`}
-                            >
-                                <span className="text-xl font-bold text-orange-500 hover:text-opacity-70 transition-all duration-300 mr-4">{post.user.name}</span>
-                            </Link>
-                            <span className="text-sm text-gray-500">
-                                {formatDistanceToNow(new Date(post.created_at), { addSuffix: true })}
-                            </span>
-                        </div>
-                        {/* Post Body */}
-                        <p className="font-medium text-gray-800">{post.body}</p>
-                        {/* Read More Link */}
-                        <Link
-                            href={`/posts/${post.id}`}
-                            className="block mt-4 text-orange-500 hover:text-orange-600 font-semibold transition duration-300 ease-in-out"
-                        >
-                            Read more...
-                        </Link>
-                    </div>
-                ))}
-                {/* Loading Spinner */}
-                {loading && (
-                    <div className="text-center mt-6">
-                        <SyncLoader
-                            color="#ff6600"
-                            loading={loading}
-                            size={20}
-                        />
-                    </div>
-                )}
+                <UserPosts posts={postData} />
             </div>
         </>
     );
